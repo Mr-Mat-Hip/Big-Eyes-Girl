61d487dec9dcdec533d4cf3fd365f440b87fdbf2
Mr-Mat-Hip <106964558+Mr-Mat-Hip@users.noreply.github.com>
2022-06-06 17:47:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    titleWeb: "Big Eyes Girl",
+    introTitle: 'Big Eyes Girl',
+    introDesc: `Yêu một người không khó
+    Vì chẳng cần lí do
+    Mọi tình cảm của anh
+    Chỉ dành cho em`,
+    btnIntro: 'Love You',
+    title: 'Phải chăng anh đã yêu em từ cái nhìn đầu tiên ',
+    desc: 'Phải chăng anh đã yêu em từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu anh',
+    btnNo: 'Không yêu',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Làm bạn gái anh nha.',
+    btnAccept: 'Em đồng ý',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
